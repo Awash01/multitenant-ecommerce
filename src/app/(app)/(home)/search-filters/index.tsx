95c7ac7f16da3ce6696e645cbcbfe6b57608c0a0
Awash01 <awashkumar017@gmail.com>
2026-01-25 20:55:29 +0530

    05: search filter

---
+++ src/app/(app)/(home)/search-filters/index.tsx
@@ -0,0 +1,21 @@
+import { SearchInput } from './search-input';
+import { Categories } from './categories';
+
+interface Props{
+
+     data: any;
+}
+
+
+
+export const SearchFilter = ({
+    data,
+    }:Props) => {
+    return(
+        <div className="px-4 lg:px-12 py-8 border-b flex flex-col gap-4 w-full">
+            <SearchInput/>
+            <Categories data={data}/>
+           
+        </div>
+    )
+}
