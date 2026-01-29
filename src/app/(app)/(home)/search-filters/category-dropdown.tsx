1f9180513bc59f0b1369bc627d979fcc3d93ac59
Awash01 <awashkumar017@gmail.com>
2026-01-29 20:50:13 +0530

    07: trpc integration

--- src/app/(app)/(home)/search-filters/category-dropdown.tsx
+++ src/app/(app)/(home)/search-filters/category-dropdown.tsx
@@ -5,10 +5,10 @@ import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { useDropdownPosition } from "./use-dropdown-position";
 import { SubCategoryMenu } from './subcategory-menu';
-import { CustomCategory } from "../types";
 import Link from "next/link";
+import { CategoriesGetManyOutput } from "@/modules/categories/types";
 interface Props{
-   category:CustomCategory;
+   category:CategoriesGetManyOutput[1]
    isActive?: boolean;
    isNavigationHovered?: boolean
 }
--- src/app/(app)/(home)/search-filters/search-input.tsx
+++ src/app/(app)/(home)/search-filters/search-input.tsx
@@ -2,7 +2,7 @@
 
 import { Input } from '@/components/ui/input';
 import { ListFilterIcon, SearchIcon } from 'lucide-react';
-import { CustomCategory } from '../types';
+
 import { CategoriesSidbar } from './categories-sidebar';
 import { useState } from 'react';
 import { Button } from '@/components/ui/button';
@@ -11,18 +11,17 @@ import { Button } from '@/components/ui/button';
 interface Props{
 
     disabled?: boolean;
-    data:CustomCategory[];
+  
 };
 
 export const SearchInput = ({
    disabled,
-   data,
 }:Props) =>{
 
    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
     return (
        <div className="flex items-center gap-2 w-full">
-         <CategoriesSidbar data={data} open={isSidebarOpen} onOpenChange={setIsSidebarOpen}/>
+         <CategoriesSidbar open={isSidebarOpen} onOpenChange={setIsSidebarOpen}/>
          <div className="relative w-full">
             <SearchIcon className='absolute left-3 top-1/2 -translate-y-1/2 size-4 text-neutral-500'/>
             <Input className='pl-8' placeholder='Search Products' disabled={disabled}/>
