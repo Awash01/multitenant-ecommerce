30a33a87e83decf8c0d67da20b7ae755a94d4564
Awash01 <awashkumar017@gmail.com>
2026-02-02 22:19:39 +0530

    10: category pages

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,46 +1,33 @@
 import { getQueryClient, trpc } from "@/trpc/server";
 
-
-
-import { Navbar } from "./navbar";
-import { Footer } from "./footer";
-import { SearchFilter, SearchFiltersSkeleton } from "./search-filters";
+import { Navbar } from "@/modules/home/ui/components/navbar";
+import { Footer } from "@/modules/home/ui/components/footer";
+import { SearchFilter, SearchFiltersSkeleton } from "@/modules/home/ui/components/search-filters";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { Suspense } from "react";
 
-
-
-
 interface Props {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 const Layout = async ({ children }: Props) => {
   const queryClient = getQueryClient();
 
-  void queryClient.prefetchQuery(
-    trpc.categories.getMany.queryOptions()
-  );
+  void queryClient.prefetchQuery(trpc.categories.getMany.queryOptions());
   return (
     <div className="flex flex-col min-h-screen">
-       <Navbar/>
-       <HydrationBoundary state={dehydrate(queryClient)}>
-        <Suspense fallback={<SearchFiltersSkeleton/>}>
+      <Navbar />
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={<SearchFiltersSkeleton />}>
           <SearchFilter />
         </Suspense>
-        
-       </HydrationBoundary>
-       
-       <div className="flex-1 bg-[#F4F4F0]">
-        {children}
-       </div> 
-      
-        <Footer/>
-      
-      
+      </HydrationBoundary>
+
+      <div className="flex-1 bg-[#F4F4F0]">{children}</div>
+
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Layout
-
+export default Layout;
