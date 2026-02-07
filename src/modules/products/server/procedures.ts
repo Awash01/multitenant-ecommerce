4d56101b91edefe30205edae23413f0610788952
Awash01 <awashkumar017@gmail.com>
2026-02-07 22:15:56 +0530

    12: filtering products

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -10,10 +10,23 @@ export const productRouter = createTRPCRouter({
     .input(
        z.object({
         category: z.string().nullable().optional(),
+        minPrice: z.string().nullable().optional(),
+        maxPrice: z.string().nullable().optional(),
        }),
   )
     .query(async ({ctx, input })=>{
         const where:Where = {};
+
+        if(input.minPrice){
+          where.price = {
+            greater_than_equal: input.minPrice
+          }
+        }
+        if(input.maxPrice){
+          where.price = {
+            less_than_equal: input.maxPrice
+          }
+        }
 
         if(input.category){
           const categoriesData = await ctx.db.find({
@@ -45,9 +58,9 @@ export const productRouter = createTRPCRouter({
             subCategoriesSlugs.push(
               ...parentCategory.subcategories.map((subcategory) => subcategory.slug)
             )
-          }
-          where["category.slug"] = {
-            in:[parentCategory.slug, ...subCategoriesSlugs],
+            where["category.slug"] = {
+              in:[parentCategory.slug, ...subCategoriesSlugs],
+            }
           }
         }
         
