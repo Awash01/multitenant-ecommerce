d1438c04a926fa591ab17c4b2eafa7f376a8bd97
Awash01 <awashkumar017@gmail.com>
2026-02-14 01:49:02 +0530

    16: tenant pages

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -4,9 +4,11 @@ import {  createTRPCRouter } from '../init';
 import { categoriesRouter } from '@/modules/categories/server/procedure';
 import { productRouter } from '@/modules/products/server/procedures';
 import { tagsRouter } from '@/modules/tags/server/procedures';
+import { tenantsRouter } from '@/modules/tenants/server/procedures';
 export const appRouter = createTRPCRouter({
     auth: authRouter,
     tags:tagsRouter,
+    tenants:tenantsRouter,
     products:productRouter,
     categories: categoriesRouter
 });
